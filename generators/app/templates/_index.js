37326059eca8e90da6df2341947cb4a54d67fec8
Stephen.Orioste <Stephen.Orioste@amiculum.biz>
2021-06-08 15:12:46 +0800

    Updated webpack config and removed some of the scss files

--- generators/app/templates/_index.js
+++ generators/app/templates/_index.js
@@ -1,13 +1,5 @@
-<% if(jsPreprocessor === 'none'){ %>
-require('./src/_styles/_fonts.scss');
-require('./src/_styles/_var.scss');
-require('./src/_styles/_styles.scss');
-require('./src/_styles/main.scss');
+<% if(jsPreprocessor === 'none'){ %>require('./src/_styles/main.scss');
 require('./src/_scripts/main.js');
-<% } else{ %>
-import './src/_styles/_fonts.scss';
-import './src/_styles/_var.scss';
-import './src/_styles/_styles.scss';
-import './src/_styles/main.scss';
+<% } else{ %>import './src/_styles/main.scss';
 import './src/_scripts/main.js';
 <% } %>
