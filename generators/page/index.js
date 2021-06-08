355f909c38a4ea3e5857e31f49843b9eb85b109e
Stephen.Orioste <Stephen.Orioste@amiculum.biz>
2021-06-08 13:56:53 +0800

    Converted space indents to tab indents

--- generators/page/index.js
+++ generators/page/index.js
@@ -2,22 +2,22 @@
 const Generator = require('yeoman-generator');
 
 module.exports = class extends Generator {
-  writing() {
-    let pages = this.config.get('pages');
-    if (pages === undefined) {
-      pages = [];
-      pages.push(this.arguments[0]);
-    } else {
-      pages.push(this.arguments[0]);
-    }
-    this.config.set('pages', pages);
+	writing() {
+		let pages = this.config.get('pages');
+		if (pages === undefined) {
+			pages = [];
+			pages.push(this.arguments[0]);
+		} else {
+			pages.push(this.arguments[0]);
+		}
+		this.config.set('pages', pages);
 
-    this.fs.copyTpl(
-      this.templatePath('page.pug'),
-      this.destinationPath(`src/${this.arguments}/index.pug`),
-      {
-        name: this.arguments
-      }
-    );
-  }
+		this.fs.copyTpl(
+			this.templatePath('page.pug'),
+			this.destinationPath(`src/${this.arguments}/index.pug`),
+			{
+				name: this.arguments
+			}
+		);
+	}
 };
